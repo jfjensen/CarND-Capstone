8438bbd1af369cadfb16f38d6570288df7fbdc78
jfjensen <jesfinkjensen@gmail.com>
2017-09-11 09:49:39 -0700

    feat: working classifier based on pure openCV

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -41,7 +41,7 @@ class TLDetector(object):
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
-        self.test_image = rospy.Publisher('/test_image', Image, queue_size=1)
+        self.test_image_pub = rospy.Publisher('/test_image', Image, queue_size=1)
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier()
@@ -77,42 +77,127 @@ class TLDetector(object):
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
 
-        self.camera_image.encoding = "rgb8"
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        rospy.loginfo("""Lights wp: {}  state: {}""".format(light_wp, state))
 
-        output = cv_image.copy()
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        # self.camera_image.encoding = "rgb8"
+        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        luminosity = hsv[:,:,2]
-        hue = hsv[:,:,0]
+        # output = cv_image.copy()
+        # # gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        # detect circles in the image
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,1,50,
-                            param1=30,param2=20,minRadius=5,maxRadius=30)
+        # hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        # # luminosity = hsv[:,:,2]
+        # # hue = hsv[:,:,0]
+
+        # # # detect circles in the image
+        # # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,1,50,
+        # #                     param1=30,param2=20,minRadius=5,maxRadius=30)
          
-        # ensure at least some circles were found
-        if circles is not None:
-            # convert the (x, y) coordinates and radius of the circles to integers
-            circles = np.round(circles[0, :]).astype("int")
+        # # # ensure at least some circles were found
+        # # if circles is not None:
+        # #     # convert the (x, y) coordinates and radius of the circles to integers
+        # #     circles = np.round(circles[0, :]).astype("int")
          
-            # loop over the (x, y) coordinates and radius of the circles
-            for (x, y, r) in circles:
-                # draw the circle in the output image, then draw a rectangle
-                # corresponding to the center of the circle
-                # https://stackoverflow.com/questions/10948589/choosing-correct-hsv-values-for-opencv-thresholding-with-inranges
-                if (luminosity[y,x] > 180):
-                    # if (hue[y,x]<40 or hue[y,x]>300):
-                    if (hue[y,x]<85 or hue[y,x]>150):
-                        cv2.circle(output, (x, y), r, (255, 0, 0), 3)
+        # #     # loop over the (x, y) coordinates and radius of the circles
+        # #     for (x, y, r) in circles:
+        # #         # draw the circle in the output image, then draw a rectangle
+        # #         # corresponding to the center of the circle
+        # #         # https://stackoverflow.com/questions/10948589/choosing-correct-hsv-values-for-opencv-thresholding-with-inranges
+        # #         if (luminosity[y,x] > 180):
+        # #             # if (hue[y,x]<40 or hue[y,x]>300):
+        # #             if (hue[y,x]<85 or hue[y,x]>150):
+        # #                 cv2.circle(output, (x, y), r, (255, 0, 0), 3)
                 
-        # output = np.hstack([cv_image, output])
+        # # output = np.hstack([cv_image, output])
 
-        # http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-        img = self.bridge.cv2_to_imgmsg(output, "bgr8")
+        # # define range of luminosity in HSV
+        # lower_lum = np.array([0,100,200])
+        # upper_lum = np.array([180,255,255])
 
-        # for debugging purposes - use 'rqt_image_view' in a separate terminal to see the image
-        self.test_image.publish(img) 
+        # # Threshold the HSV image to get only higher luminosity
+        # lum_mask = cv2.inRange(hsv, lower_lum, upper_lum)
+
+        # # Bitwise-AND mask and original image
+        # output = cv2.bitwise_and(output,output, mask= lum_mask)
+
+        # # define range of blue in HSV
+        # lower_blue = np.array([80,0, 0])
+        # upper_blue = np.array([160,255,255])
+
+        # # Threshold the HSV image to get only higher luminosity
+        # blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        # not_blue_mask = cv2.bitwise_not(blue_mask)
+
+        # # Bitwise-AND mask and original image
+        # output = cv2.bitwise_and(output,output, mask= not_blue_mask)
+
+        
+        # # # detect circles in the image
+        # # gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+        # hue = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)[:,:,0]
+        # # circles = cv2.HoughCircles(hue, cv2.HOUGH_GRADIENT,1,15,
+        # #                     param1=30,param2=15,minRadius=1,maxRadius=30)
+         
+        # # # ensure at least some circles were found
+        # # if circles is not None:
+        # #     # convert the (x, y) coordinates and radius of the circles to integers
+        # #     circles = np.round(circles[0, :]).astype("int")
+         
+        # #     # loop over the (x, y) coordinates and radius of the circles
+        # #     for (x, y, r) in circles:
+        # #         # draw the circle in the output image, then draw a circle
+        # #         # corresponding to the center of the circle
+        # #         # https://stackoverflow.com/questions/10948589/choosing-correct-hsv-values-for-opencv-thresholding-with-inranges
+        # #         cv2.circle(output, (x, y), r, (255, 0, 0), 3)
+                
+        # # output = np.hstack([cv_image, output])
+
+        # count_lreds = np.count_nonzero(cv2.inRange(hue,1,19))
+        # count_ureds = np.count_nonzero(cv2.inRange(hue,160,180))
+        # count_reds = count_lreds + count_ureds
+        # count_yellows = np.count_nonzero(cv2.inRange(hue,20,35))
+        # count_greens = np.count_nonzero(cv2.inRange(hue,40,80))
+
+        # count_vec = [count_reds, count_yellows, count_greens]
+        # max_count_ix = np.argmax(count_vec)
+        # max_val = count_vec[max_count_ix]
+
+        # lights_detected = 'None'
+
+        # if (max_val > 120):
+        #     if (max_count_ix == 0):
+        #         lights_detected = 'Red'
+        #         self.state = TrafficLight.RED
+        #     elif (max_count_ix == 1):
+        #         lights_detected = 'Yellow'
+        #         self.state = TrafficLight.YELLOW
+        #     elif (max_count_ix == 2):
+        #         lights_detected = 'Green'
+        #         self.state = TrafficLight.GREEN
+        # else:
+        #     lights_detected = 'None'
+        #     self.state = TrafficLight.UNKNOWN
+
+
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # reds_txt    = 'Reds:    {}'.format(count_reds)
+        # yellows_txt = 'Yellows: {}'.format(count_yellows)
+        # greens_txt  = 'Greens:  {}'.format(count_greens)
+
+        # cv2.putText(output,reds_txt,(10,50), font, 1,(255,255,255),2)
+        # cv2.putText(output,yellows_txt,(10,100), font, 1,(255,255,255),2)
+        # cv2.putText(output,greens_txt,(10,150), font, 1,(255,255,255),2)
+
+        # lights_txt = lights_detected + ' traffic lights detected!'
+        # cv2.putText(output,lights_txt,(10,550), font, 1,(255,255,255),2)
+
+        # # http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+        # img = self.bridge.cv2_to_imgmsg(output, "bgr8")
+        
+        # # for debugging purposes - use 'rqt_image_view' in a separate terminal to see the image
+        # self.test_image.publish(img)
+        # rospy.loginfo("""Lights detected {}""".format(lights_detected))
 
         '''
         Publish upcoming red lights at camera frequency.
@@ -143,7 +228,32 @@ class TLDetector(object):
 
         """
         #TODO implement
-        return 0
+        waypoints = self.waypoints.waypoints
+
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2)#  + (a.z-b.z)**2)
+
+        closest_len = 100000
+        
+        
+        closest_waypoint = 0 
+        next_waypoint = 0
+            
+        num_waypoints = self.num_waypoints
+        dist = dl(waypoints[closest_waypoint].pose.pose.position, pose.position)
+
+        while (dist < closest_len) and (closest_waypoint < num_waypoints):
+            closest_waypoint = next_waypoint
+            closest_len = dist
+            dist = dl(waypoints[closest_waypoint+1].pose.pose.position, pose.position)
+            next_waypoint += 1
+
+        dist_prev = dl(waypoints[closest_waypoint-1].pose.pose.position, pose.position)
+        dist_curr = dl(waypoints[closest_waypoint].pose.pose.position, pose.position)
+        dist_next = dl(waypoints[closest_waypoint+1].pose.pose.position, pose.position)
+
+        # rospy.loginfo("""Light detection -> waypoint dist {} {} {}""".format(dist_prev, dist_curr, dist_next))
+
+        return closest_waypoint
 
 
     def project_to_image_plane(self, point_in_world):
@@ -200,9 +310,12 @@ class TLDetector(object):
         self.camera_image.encoding = "rgb8"
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        x, y = self.project_to_image_plane(light.pose.pose.position)
+        # x, y = self.project_to_image_plane(light.pose.pose.position)
 
         #TODO use light location to zoom in on traffic light in image
+        # for debugging purposes - use 'rqt_image_view' in a separate terminal to see the image
+        img = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+        self.test_image_pub.publish(img)
 
         #Get classification
         return self.light_classifier.get_classification(cv_image)
@@ -223,6 +336,7 @@ class TLDetector(object):
         """
         light = None
         light_positions = config.light_positions
+        light_wp = None
         if(self.pose and self.waypoints):
             closest_waypoint_index = self.get_closest_waypoint(self.pose.pose)
             closest_waypoint_ps = self.waypoints.waypoints[closest_waypoint_index].pose
@@ -235,6 +349,14 @@ class TLDetector(object):
                 if distance < closest_light_distance:
                     closest_light_distance = distance
                     closest_light_position = light_position
+
+                    light = True
+                    light_pose = Pose()
+                    light_pose.position.x = light_position[0]
+                    light_pose.position.y = light_position[1]
+                    light_pose.position.z = 0 ####################
+                    light_wp = self.get_closest_waypoint(light_pose)
+
 
         if light:
             state = self.get_light_state(light)
